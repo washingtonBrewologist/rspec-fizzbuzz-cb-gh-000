65ac2a6d7ffd7cbbbdb3572fabb7243a92dfb0de
Nick Williams <nwilliams7539@student.whatcom.edu>
2018-09-14 19:25:36 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,13 +3,9 @@
 def fizzbuzz(int)
 if int % 5 == 0 && int % 3 == 0
   puts "FizzBuzz"
-elsif int % 3 == 0
-  puts "Fizz"
-elsif int % 5 == 0
-  puts "Buzz"
+#elsif int % 3 == 0
+#  puts "Fizz"
+#elsif int % 5 == 0
+#  puts "Buzz"
   end
 end
-fizzbuzz(3)
-fizzbuzz(5)
-fizzbuzz(15)
-fizzbuzz(4)
