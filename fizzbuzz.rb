2b2b7e6959d50d981babac02b556372a4345a609
Nick Williams <nwilliams7539@student.whatcom.edu>
2018-09-14 19:29:50 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -8,14 +8,5 @@ elsif int % 5 == 0
 elsif (int % 5 == 0) && (int % 3 == 0)
   "FizzBuzz"
 else
-end
-
-
-#if int % 5 == 0 && int % 3 == 0
-#  puts "FizzBuzz"
-#elsif int % 3 == 0
-#  puts "Fizz"
-#elsif int % 5 == 0
-#  puts "Buzz"
   end
 end
