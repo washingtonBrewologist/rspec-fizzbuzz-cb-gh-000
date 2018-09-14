947d2e123ee27cf0f086b64305721f458e751964
Nick Williams <nwilliams7539@student.whatcom.edu>
2018-09-14 18:35:30 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
-  if (number % 3 == 0)
-    puts "Fizz"
+  if (number % 3 == 0 && number % 5 == 0)
+    puts "FizzBuzz"
   elsif (number % 5 == 0)
     puts "Buzz"
-  elsif (number % 3 == 0 && number % 5 == 0)
-    puts "FizzBuzz"
+  elsif (number % 3 == 0)
+    puts "Fizz"
   else
     unless(number % 2 == 0)
     end
