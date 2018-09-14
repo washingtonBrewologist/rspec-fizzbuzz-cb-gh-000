ed243ae6dcfad91b117069e5a943bcd382774547
Nick Williams <nwilliams7539@student.whatcom.edu>
2018-09-14 18:54:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    puts "FizzBuzz"
-  elsif number % 5 == 0
-    puts "Buzz"
-  elsif number % 3 == 0
-    puts "Fizz"
+def fizzbuzz(int)
+  if int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  elsif int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
   else
-    unless(number % 2 == 0)
+    unless int % 3 != 0 || int % 5 != 0
     end
-  end
 end
-#fizzbuzz(6)
+fizzbuzz(6)
