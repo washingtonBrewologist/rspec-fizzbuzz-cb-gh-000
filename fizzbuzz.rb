71b3947bdea59c079a7a401db7bbe97b49913959
Nick Williams <nwilliams7539@student.whatcom.edu>
2018-09-14 19:09:15 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
-    puts "FizzBuzz"
-  elsif int % 3 == 0
+  if int % 3 == 0
     puts "Fizz"
   elsif int % 5 == 0
     puts "Buzz"
+  elsif int % 3 == 0 && int % 5 == 0
+    puts "FizzBuzz"
   end
 end
-fizzbuzz(4)
